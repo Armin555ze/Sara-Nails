98590dbec28fb86bcee41b3c5f561a2faec4f681
Armin555ze <107627115+Armin555ze@users.noreply.github.com>
2022-10-13 22:04:57 +0200

    ghnf

--- Sara-nails/nokti 2/script.js
+++ Sara-nails/nokti 2/script.js
@@ -1,10 +1,4 @@
-form.addEventListener('submit', e => {
-    e.preventDefault()
-    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-    .then(response => console.log('Success!', response))
-    .catch(error => console.error('Error!', error.message))
-    })
-   $('#myform').reset(); // attempt to reset the form
+
 
 
    
